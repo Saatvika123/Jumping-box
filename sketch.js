c1337f83b8bbcdbe0b212b1f792e9fc19eb6d72e
Saatvika123 <77545701+Saatvika123@users.noreply.github.com>
2021-03-07 11:00:02 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,75 @@
-var canvas;
-var music;
-
+var sprite1, sprite2, sprite3, sprite4
+var movingSprite
+var music
 function preload(){
     music = loadSound("music.mp3");
 }
 
 
 function setup(){
-    canvas = createCanvas(800,600);
+    canvas = createCanvas((800,600);
+movingSprite=createSprite(random(10,750)300,20,20)
+movingSprite.shapeColor=("white
+"")
+movingSprite.velocityX=3;
+movingSprite.velocityY=3;
 
-    //create 4 different surfaces
+sprite1=createSprite(100,598,180,20)
+sprite1.shapeColor=("red")
 
+sprite2=createSprite(300,596,180,20)
+sprite2.shapeColor=("green")
 
+sprite3=createSprite(500,600,180,20)
+sprite3.shapeColor=("blue")
 
-    //create box sprite and give velocity
+sprite4=createSprite(700,590,180,20)
+sprite4.shapeColor=("yellow")
 
 }
 
+
 function draw() {
     background(rgb(169,169,169));
-    //create edgeSprite
+  
+if(movingSprite.x<0){
+music.stop()
+movingSprite.velocity=3
+}else if(movingSprite.x>800){
+music.stop()
+movingSprite.velocityX=-3
 
 
 
-    //add condition to check if box touching surface and make it box
+if(isTouching(movingSprite,sprite4)){
+music.play()
+movingSprite.shapeColor=("yellow")
+bounceOff(movingSprite,sprite4)
+}
+
+else if(isTouching(movingSprite,sprite3)){
+music.stop()
+movingSprite.shapeColor=("blue")
+bounceOff(movingSprite,sprite3)
+}
+
+else if(isTouching(movingSprite,sprite2)){
+music.stop()
+movingSprite.shapeColor=("green")
+movingSprite.velocityX=0
+movingSprite.velocityY=0
+}
+
+else if(isTouching(movingSprite,sprite1)){
+    music.stop()
+    movingSprite.shapeColor=("red")
+    bounceOff(movingSprite,fixSprite1)
+}
+
+if(movingSprite.y<0){
+music.stop()
+movingSprite.velocityY=3
+}
+
+drawSprites()
 }
