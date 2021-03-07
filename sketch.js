7c3fa48b1ef5fc3b1ebc9d4746e7e243b714ac2a
Saatvika123 <77545701+Saatvika123@users.noreply.github.com>
2021-03-07 10:57:17 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,27 @@
-var sprite1, sprite2, sprite3, sprite4
-var movingSprite
-var music
+var canvas;
+var music;
 
-
-
-
-function preload() {
-cat=loadAnimation("cat1.png","cat2.png","cat3.png")
-mouse=loadAnimation("mouse1.png","mouse2.png","mouse3.png", "mouse4.png")
+function preload(){
+    music = loadSound("music.mp3");
 }
 
+
 function setup(){
-    createCanvas(1000,800);
-   cat=createSprite(200,300,50,50
-   mouse=createSprite(500,20,30,20)
-   mouse.addAnimation(mouse1.png, "mouse")
-  
+    canvas = createCanvas(800,600);
+
+    //create 4 different surfaces
+
+
+
+    //create box sprite and give velocity
 
 }
 
 function draw() {
-
-    background("white");
-    Text(mouseX+','+mouseY, 10, 45);
-   if(cat.x-mouse.x<(cat.width-mouse.width)/2)
-    cat.addAnimation("catLastImage",catImg3);
-    cat.changeAnimation("catLastImage");
-    drawSprites();
-}
+    background(rgb(169,169,169));
+    //create edgeSprite
 
 
-function keyPressed(){
-if(keyCode==LEFT ARROW){
-cat.velocityX=-5;
-cat.addAnimation("catRunning",catImg2)
-cat.changeAnimation("catRunning");
-}
-  
 
+    //add condition to check if box touching surface and make it box
 }
